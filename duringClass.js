27a3dccbb474f9aa00f67bb5d0861c6bdcaff204
A Lee <ayslee@gmail.com>
2018-01-16 22:54:40 +0000

    automatically backed up by learn

---
+++ duringClass.js
@@ -0,0 +1,10 @@
+
+var burgers = ["Bob", "Linda", "Tina", "Louise", "Gene"];
+function printNames (array) {
+  for(i = 0; i < array.length; i++) {
+    console.log(burgers[i]);
+  }
+}
+
+printNames(burgers);
+

94291b2afd9b3683cbaf89e695b7760cfb98f2e1
A Lee <ayslee@gmail.com>
2018-01-16 22:53:43 +0000

    automatically backed up by learn

--- looping.js
+++ looping.js
@@ -4,9 +4,18 @@ console.log(names[0])
 console.log(names[1])
 console.log(names[2])
 
-for(i = 0; i < names.length; i++, ) {
-  console.log(name)
+for(i = 0; i < names.length; i++; ) {
+  console.log(names[i]);
 }
+
+var burgers = ["Bob", "Linda", "Tina", "Louise", "Gene"];
+function printNames (array) {
+  for(i = 0; i < array.length; i++) {
+    console.log(burgers[i]);
+  }
+}
+
+printNames(burgers);
 
 
 while(condition) {

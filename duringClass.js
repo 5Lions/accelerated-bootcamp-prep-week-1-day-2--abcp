39161a4c1ca9058eb0ecc05ff661801536d32876
A Lee <ayslee@gmail.com>
2018-01-16 22:55:00 +0000

    automatically backed up by learn

--- duringClass.js
+++ duringClass.js
@@ -1,4 +1,14 @@
 
+function someFunction (num) {
+  if (num > 10) {
+    return num;
+  } else {
+    return "Sorry that number isn't greater than 10!";
+  }
+}
+console.log(someFunction(5));
+
+
 var burgers = ["Bob", "Linda", "Tina", "Louise", "Gene"];
 function printNames (array) {
   for(i = 0; i < array.length; i++) {
